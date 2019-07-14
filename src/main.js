5bad30fed8d3be6756110f6f07f94d8fe231f14d
Tr-blank <blank0129@gmail.com>
2019-07-14 15:50:46 +0800

    :sparkles: Feat: add done list

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ Vue.config.productionTip = false
 const apiConfig = {
   apiKey: 'AIzaSyAY4THLuVYiFhrRAPZCseQDbCO3bxNJD9A',
   clientId: '624530488961-chtl120tenn14g9i8elqlj5m49888nau.apps.googleusercontent.com',
-  discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
+  discoveryDocs: ['https://content.googleapis.com/discovery/v1/apis/sheets/v4/rest'],
   scope: 'https://www.googleapis.com/auth/spreadsheets' // See, edit, create, and delete your spreadsheets in Google Drive
   // see all available scopes here: https://developers.google.com/identity/protocols/googlescopes'
 }
